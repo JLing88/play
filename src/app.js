bda0085ed78586611a6a8f2ee69358e32ab1dd5d
Mesky <dmeskis@gmail.com>
2018-12-04 11:32:27 -0700

    Can get songs

--- src/app.js
+++ src/app.js
@@ -12,8 +12,19 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Play';
 
 app.get('/', (request, response) => {
-  response.send('Damn');
+  response.send('Place holder');
 });
+
+app.get('/api/v1/songs', (request, response) => {
+  database('songs').select()
+    .then((papers) => {
+      response.status(200).json(papers);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
