4efad3d563fc692e802deb92e487945a271130b0
Mesky <dmeskis@gmail.com>
2018-12-03 20:30:43 -0700

    Can create a song

--- src/app.js
+++ src/app.js
@@ -1,21 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 app.use(cors())
-
-app.get('/songs', (req, res) => {
-  res.send(
-    [{
-      title: "Hello World!",
-      description: "Hi there! How are you?"
-    }]
-  )
-})
 
 app.listen(process.env.PORT || 8081)
 
@@ -26,3 +19,66 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
   console.log("Connection Succeeded");
 });
+
+
+var Song = require("../models/song");
+
+app.get('/songs', (req, res) => {
+  Song.find({}, 'name artist_name genre song_rating', function (error, songs) {
+    if (error) { console.error(error); }
+    res.send({
+      songs: songs
+    })
+  }).sort({_id:-1})
+})
+
+// Add new song
+// app.post('/songs', (req, res) => {
+//   var db = req.db;
+//   var name = req.body.name;
+//   var artist_name = req.body.description;
+//   var genre = req.body.genre;
+//   var song_rating = req.body.song_rating;
+//   var new_song = new Song({
+//     name: name,
+//     artist_name: artist_name,
+//     genre: genre,
+//     song_rating: song_rating
+//   });
+//
+//   new_song.save(function (error) {
+//     if (error) {
+//       console.log(error)
+//     }
+//     res.send({
+//       success: true,
+//       message: 'Song saved succesfully!'
+//     })
+//   })
+// })
+
+app.post('/songs', (req, res) => {
+  console.log(req.body)
+  var db = req.db;
+  var name = req.body.name;
+  var artist_name = req.body.artist_name;
+  var genre = req.body.genre;
+  var song_rating = req.body.song_rating;
+  console.log(song_rating)
+  var new_song = new Song({
+    name: name,
+    artist_name: artist_name,
+    genre: genre,
+    song_rating: song_rating
+  })
+
+  new_song.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Song saved successfully!'
+    })
+  })
+})
