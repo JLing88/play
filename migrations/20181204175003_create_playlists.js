9d5f4fd1e0cc2f5060c57514c0a013ab8b84f252
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-12-04 18:03:51 -0700

    Add playlists and playlist_songs tables

--- migrations/20181204175003_create_playlists.js
+++ migrations/20181204175003_create_playlists.js
@@ -1,11 +1,9 @@
+
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('songs', function(table) {
+    knex.schema.createTable('playlists', function(table) {
       table.increments('id').primary();
       table.string('name').notNullable();
-      table.string('artist_name').notNullable();
-      table.string('genre').notNullable();
-      table.integer('song_rating');
 
       table.timestamps(true, true);
     })
@@ -14,6 +12,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('songs'),
-  ]);
-}
+    knex.schema.dropTable('songs')
+  ])
+};
