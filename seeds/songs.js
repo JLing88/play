cf1aec1d36e0ab1d0c0736bbcd3761a466dcd65c
Mesky <dmeskis@gmail.com>
2018-12-05 19:05:12 -0700

    Create seed files

--- seeds/songs.js
+++ seeds/songs.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
         {name: 'Hot in Here', artist_name: 'Nelly', genre: 'Hip Hop', song_rating: 7},
         {name: 'Blurred Lines', artist_name: 'Robin Thicke', genre: 'Disco', song_rating: 93},
         {name: 'Umbrella', artist_name: 'Rihanna', genre: 'Pop', song_rating: 46},
-        {name: 'Umbrella', artist_name: 'Rihanna', genre: 'Pop', song_rating: 75},
+        {name: 'U Remind Me', artist_name: 'Usher', genre: 'R&B', song_rating: 75}
       ]);
     });
 };
